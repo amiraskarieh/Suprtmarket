228a062eaa2f517afe00c1d5a6740f31b15a59ce
SAMoosavi <Moosavi238@gmail.com>
2024-05-13 08:51:22 +0330

    edit layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,11 @@ import {createApp, h} from 'vue';
 import {createInertiaApp} from '@inertiajs/vue3';
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 import Toast from "vue-toastification";
+import {createPinia} from 'pinia'
 import {ZiggyVue} from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pinia = createPinia()
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
@@ -17,6 +19,7 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue)
             .use(Toast, {})
+            .use(pinia)
             .mount(el);
     },
     progress: {
