83feb1cd2f7ba3c210f9caf68b9467de921e8472
SAMoosavi <Moosavi238@gmail.com>
2024-05-23 13:28:42 +0330

    add count

--- database/migrations/2024_05_23_090642_create_supplieds_table.php
+++ database/migrations/2024_05_23_090642_create_supplieds_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
 
+            $table->bigInteger('count');
             $table->timestamps();
         });
     }
